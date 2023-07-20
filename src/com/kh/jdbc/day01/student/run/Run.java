e7c72d23bc670b7188dcd82133a02c6e495bc398
siuuuuuuuu <siuuuuuuuu515@gmail.com>
2023-07-20 17:37:05 +0900

    [INIT] 23/07/20 JDBC 프로그래밍 개념 및 실습
    1. JDBC 드라이버, ojdbc6.jar ADD 중요
     - Class.forName()
    2. JDBC 코딩절차에 따른 Class, Interface 사용
     - DriverManager, Connection, Statement, ResultSet(SELECT)
    3. 후처리의 중요성
     - next(), getString(), getInt(), getDate(),...
    4. 학생관리 프로그램 MVC 패턴 적용 후 JDBC 프로그래밍 완료(학생 전체정보 조회)

---
+++ src/com/kh/jdbc/day01/student/run/Run.java
@@ -0,0 +1,13 @@
+package com.kh.jdbc.day01.student.run;
+
+import com.kh.jdbc.day01.student.view.StudentView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StudentView view = new StudentView();
+		view.startProgram();
+	}
+
+}
